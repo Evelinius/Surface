f2cb5ab49eb2f8b711f8d8b5250f99a2115f3fdc
Eveline <monsterelle9@gmail.com>
2019-06-12 17:21:34 +0300

    Переменные переименованы

--- Surface/Program.cs
+++ Surface/Program.cs
@@ -12,60 +12,60 @@ namespace Surface
         public bool IsLake;
         public bool Processed;
         public int LakeId;
-
     }
-   
 
     class Program
     {
         static int H, L;
         static List<Point> coors = new List<Point>();
         static Dictionary<int, int> Squares = new Dictionary<int, int>();
-        static int Algorithm(Surface[,] map, int x, int y, int lakeID)
+        static int Algorithm(Surface[,] map, int x, int y, int lakeId)
         {
-            List<Point> CoordinatesToProcess = new List<Point>();
-            CoordinatesToProcess.Add(new Point (x, y));
-            map[x, y].Processed = true;
-            map[x, y].LakeId = lakeID;
-
-            for (int j = 0; j < CoordinatesToProcess.Count; j++)
+            List<Point> coordinatesToProcess = new List<Point>
             {
-                var coordinate = CoordinatesToProcess[j];
+                new Point(x, y)
+            };
+            map[x, y].Processed = true;
+            map[x, y].LakeId = lakeId;
+
+            for (int j = 0; j < coordinatesToProcess.Count; j++)
+            {
+                var coordinate = coordinatesToProcess[j];
                 int xcurrent = coordinate.X;
                 int ycurrent = coordinate.Y;
                 if ((L > xcurrent + 1) && map[xcurrent + 1, ycurrent].IsLake && !map[xcurrent + 1, ycurrent].Processed)
                 {
-                    CoordinatesToProcess.Add(new Point (xcurrent + 1, ycurrent));
-                    map[xcurrent + 1, ycurrent].LakeId = lakeID;
+                    coordinatesToProcess.Add(new Point (xcurrent + 1, ycurrent));
+                    map[xcurrent + 1, ycurrent].LakeId = lakeId;
                     map[xcurrent + 1, ycurrent].Processed = true;
                 }
                 if ((xcurrent - 1 >= 0) && map[xcurrent - 1, ycurrent].IsLake && !map[xcurrent - 1, ycurrent].Processed)
                 {
-                    CoordinatesToProcess.Add(new Point (xcurrent - 1, ycurrent));
-                    map[xcurrent - 1, ycurrent].LakeId = lakeID;
+                    coordinatesToProcess.Add(new Point (xcurrent - 1, ycurrent));
+                    map[xcurrent - 1, ycurrent].LakeId = lakeId;
                     map[xcurrent - 1, ycurrent].Processed = true;
                 }
                 if ((H > ycurrent + 1) && map[xcurrent, ycurrent + 1].IsLake && !map[xcurrent, ycurrent + 1].Processed)
                 {
-                    CoordinatesToProcess.Add(new Point (xcurrent, ycurrent + 1));
-                    map[xcurrent, ycurrent + 1].LakeId = lakeID;
+                    coordinatesToProcess.Add(new Point (xcurrent, ycurrent + 1));
+                    map[xcurrent, ycurrent + 1].LakeId = lakeId;
                     map[xcurrent, ycurrent + 1].Processed = true;
                 }
                 if ((ycurrent - 1 >= 0) && map[xcurrent, ycurrent - 1].IsLake && !map[xcurrent, ycurrent - 1].Processed)
                 {
-                    CoordinatesToProcess.Add(new Point (xcurrent, ycurrent - 1));
-                    map[xcurrent, ycurrent - 1].LakeId = lakeID;
+                    coordinatesToProcess.Add(new Point (xcurrent, ycurrent - 1));
+                    map[xcurrent, ycurrent - 1].LakeId = lakeId;
                     map[xcurrent, ycurrent - 1].Processed = true;
                 }
             }
-            return CoordinatesToProcess.Count;
+            return coordinatesToProcess.Count;
         }
 
         static void Main(string[] args)
         {
             L = int.Parse(Console.ReadLine());
             H = int.Parse(Console.ReadLine());
-            Surface[,] map = new Surface[L, H];
+            var map = new Surface[L, H];
 
             for (int i = 0; i < H; i++)
             {
@@ -80,9 +80,9 @@ namespace Surface
             for (int i = 0; i < N; i++)
             {
                 string[] inputs = Console.ReadLine().Split(' ');
-                int X = int.Parse(inputs[0]);
-                int Y = int.Parse(inputs[1]);
-                coors.Add(new Point (X, Y));
+                int x = int.Parse(inputs[0]);
+                int y = int.Parse(inputs[1]);
+                coors.Add(new Point (x, y));
             }
 
             for (int i = 0; i < N; i++)
@@ -106,7 +106,6 @@ namespace Surface
                         Console.WriteLine("0");
                     }
                 }
-
             }
         }
 
